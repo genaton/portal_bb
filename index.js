d5680141fcd18811f03e7ffc76e2d09ca9749d0f
genaton <genaton@gmail.com>
2025-03-25 14:49:40 -0300

    Ativa swiper-button-next e swiper-button-prev

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ const swiper = new Swiper('.swiper', {
         type: 'bullets',
     },
 });
+
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+// import Swiper and modules styles
+import 'swiper/swiper.min.css';
+import 'swiper/modules/navigation.min.css';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
+  ....,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// OR (Alternative)
+
+// const swiper = new Swiper('.swiper', {
+//   modules: [Navigation],
+//   ....,
+// });
+// swiper.nextEl = '.swiper-button-next';
+// swiper.prevEl = '.swiper-button-prev';
