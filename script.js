e5d58a1cac03ab05fd131df6d6773b5f41afb13d
Renan <renan.paschoalotti@gmail.com>
2025-03-26 21:33:06 -0300

    correcao no 2 carrosel, edicao dos estilos e texto acompanhando o slide

--- script.js
+++ script.js
@@ -23,3 +23,39 @@ document.getElementById('formulario').addEventListener('submit', function(event)
     document.getElementById('formulario').style.display = 'none';
     document.getElementById('mensagemSucesso').style.display = 'block';
 });
+
+
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 1.8, // Mostra 3 slides ao mesmo tempo
+    spaceBetween: 20, // Espaço de 20px entre eles
+    centeredSlides: true, // Centraliza o slide ativo
+    loop: false,
+    pagination: {
+        effect: "slide",
+  /* ou "coverflow" para efeito 3D */
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+});
+
+var swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 2,
+    spaceBetween: 70,
+    centeredSlides: true,
+    loop: false,
+    pagination: {
+        effect: "coverflow",
+        el: '.swiper2 .swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper2 .swiper-button-next',
+        prevEl: '.swiper2 .swiper-button-prev'
+    }
+});
+
+
